5d0bbbed75926dc1a839d1f0aa47d2ca483d5088
Raghav <Raghav.intrigue@gmail.com>
2017-06-02 22:26:54 +0530

    - Added LoginActivity - Added ProfileActivity - Added NavigationDrawer

---
+++ app/src/main/java/com/blackboxindia/keepit/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.blackboxindia.keepit.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blackboxindia.keepit.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
--- app/src/main/java/com/blackboxindia/keepit/dataModels/AdDataMini.java
+++ app/src/main/java/com/blackboxindia/keepit/dataModels/AdDataMini.java
@@ -30,6 +30,4 @@ public class AdDataMini {
 
 
 
-
-
 }
